39facc74d081c0bdbb307185391b5bdd96590760
Mtzallqmy <mtzallqmy@gmail.com>
2026-09-07 01:38:14 +0300

    build: use stable Android 16 SDK baseline

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.moataz.edge"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.moataz.edge"
@@ -41,7 +41,7 @@ chaquopy {
 }
 
 dependencies {
-    val composeBom = platform("androidx.compose:compose-bom:2026.08.00")
+    val composeBom = platform("androidx.compose:compose-bom:2026.04.01")
     implementation(composeBom)
 
     implementation("androidx.activity:activity-compose:1.13.0")
